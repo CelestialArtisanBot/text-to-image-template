e8494869a3fbe2cb1901182e3c794133386ac93e
Celestialartisanbot <celestialartisanbot@gmail.com>
2025-08-30 08:42:07 -0500

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -15,7 +15,7 @@ no bra or panties on",
 
     return new Response(response, {
       headers: {
-        "content-type": "image/png/mp4",
+        "content-type": "image/png/GIF",
       },
     });
   },
