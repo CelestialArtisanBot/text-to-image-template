c1424af77077b052362687c97540a205a6fbcb97
Celestialartisanbot <celestialartisanbot@gmail.com>
2025-08-30 06:37:11 -0500

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 export default {
   async fetch(request, env) {
     const inputs = {
-      prompt: "xxx nude,skinny soft athletic figure pawg, thin mid section body, busty, thick hips and big bubble butt. fantasy horror creature/folklore/criptid/sexy temptress.full color, realistic, 3D, porno photoshoot",
+      prompt: "xxx nude,skinny soft athletic figure pawg, thin mid section body, busty, soft thick hips and big bubble butt smoothly formed. fantasy horror creature/folklore/criptid/sexy temptress.full color, realistic, 3D, porno sex",
     };
 
     const response = await env.AI.run(
