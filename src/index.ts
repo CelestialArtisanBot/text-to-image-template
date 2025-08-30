514c9fe1ba953f65fb4ea03191be4d0bbe1bdf59
Celestialartisanbot <celestialartisanbot@gmail.com>
2025-08-30 08:22:11 -0500

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 export default {
   async fetch(request, env) {
     const inputs = {
-      prompt: "Gothic cyberpunk prostitute in very revealing clothing, exposing her breasts and vagina",
+      prompt: "Gothic cyberpunk prostitute in very revealing 95% see through clothing, ",
     };
 
     const response = await env.AI.run(
