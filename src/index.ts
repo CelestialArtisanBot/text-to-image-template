352b36958d29a8014ce7fad6a39a82f9a8e963e0
Celestialartisanbot <celestialartisanbot@gmail.com>
2025-08-30 07:30:51 -0500

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 export default {
   async fetch(request, env) {
     const inputs = {
-      prompt: "rough anal sex",
+      prompt: "Hentai, popular cartoos",
     };
 
     const response = await env.AI.run(
