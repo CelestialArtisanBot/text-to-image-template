fcc088f67880a983c19743ff698e360832c2d661
Celestialartisanbot <celestialartisanbot@gmail.com>
2025-08-30 06:27:22 -0500

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 export default {
   async fetch(request, env) {
     const inputs = {
-      prompt: "xxx no clothes on, curvy soft athletic figure pawg, thin muscular mid section body, busty, thick hips and big bubble butt. fantasy horror creature/folklore/criptid/sexy temptress.
+      prompt: "xxx no clothes on,thin soft athletic figure pawg, thin muscular mid section body, busty, thick hips and big bubble butt. fantasy horror creature/folklore/criptid/sexy temptress.
 full color, realistic, 3D, photoshoot",
     };
 
