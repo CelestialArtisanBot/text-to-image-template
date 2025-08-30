44b5c14ef042cf08148b09b939fc2729144d8417
Celestialartisanbot <celestialartisanbot@gmail.com>
2025-08-30 06:58:32 -0500

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 export default {
   async fetch(request, env) {
     const inputs = {
-      prompt: "pawg thot ho thick&thin backshot stripper hooker",
+      prompt: "nude pawg thot ho thick&thin backshot stripper hooker",
     };
 
     const response = await env.AI.run(
