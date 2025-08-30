d5286a0c84a0243fda1376d3bb01d981bb2c06ab
Celestialartisanbot <celestialartisanbot@gmail.com>
2025-08-30 08:01:38 -0500

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 export default {
   async fetch(request, env) {
     const inputs = {
-      prompt: "Gothic nude nympho inserting fingers into her asshole, penetration",
+      prompt: "Gothic nude nympho inserting fingers into her own anus, penetration",
     };
 
     const response = await env.AI.run(
