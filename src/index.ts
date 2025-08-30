44c592a30a37f76540d51768a57b43bcc2936a3f
Celestialartisanbot <celestialartisanbot@gmail.com>
2025-08-30 07:52:50 -0500

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 export default {
   async fetch(request, env) {
     const inputs = {
-      prompt: "Gothic nude nympho inserting a large carrot into her asshole",
+      prompt: "Gothic nude nympho inserting a large carrot into her asshole penni training",
     };
 
     const response = await env.AI.run(
