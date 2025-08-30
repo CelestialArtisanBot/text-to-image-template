b870560a87ad75f26dec9de25ad0b2093fe1b39d
Celestialartisanbot <celestialartisanbot@gmail.com>
2025-08-30 08:06:21 -0500

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 export default {
   async fetch(request, env) {
     const inputs = {
-      prompt: "Gothic beautiful nude nymphomaniac pawg",
+      prompt: "Gothic beautiful nude nymphomaniac pawg unclothed",
     };
 
     const response = await env.AI.run(
